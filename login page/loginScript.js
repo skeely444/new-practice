ba7ad71e617c72af5623fd063a75f165175a0631
skeely444 <mauro.camerlinckx@telenet.be>
2025-03-02 16:18:42 +0100

    i want to give up

--- login page/loginScript.js
+++ login page/loginScript.js
@@ -1,7 +1,9 @@
 function login() {
   let inputtedPassword = document.getElementById("password").value;
-  const correctPassword = ['test', 'test123', 'hi'];
-  if (inputtedPassword === correctPassword) {
+  const correctPassword = 'test'
+  const  inputtedUsername = document.getElementById("username").value;
+  const correctUsername  = "skeely"
+  if (inputtedPassword === correctPassword && inputtedUsername === correctUsername) {
     alert("hi")
   }
 } 
